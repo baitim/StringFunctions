31a3853038075840ba7ee6eafd078e033b3e5518
Тимур Байдюсенов <timur_baid@mail.ru>
2023-08-25 21:18:20 +0300

    refactor

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@
 
 int main ()
 {
-        printf(ANSI_LIGHT_BLUE "# Square equation solver\n"
+        printf(ANSI_LIGHT_BLUE "# Implementation of string functions\n"
                                "# (c) BAIDIUSENOV TIMUR, 2023\n\n" ANSI_DEFAULT_COLOR);
 
         return 0;
