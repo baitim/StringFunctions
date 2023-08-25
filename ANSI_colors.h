d8ea1d5eb7fdafa52b86c62c0f07fe522575ba46
Тимур Байдюсенов <timur_baid@mail.ru>
2023-08-25 21:06:49 +0300

    added Main.cpp

---
+++ ANSI_colors.h
@@ -0,0 +1,12 @@
+#ifndef ANSI_COLORS_H
+#define ANSI_COLORS_H
+
+#define ANSI_DEFAULT_COLOR   "\033[0m"
+#define ANSI_YELLOW          "\033[33m"
+#define ANSI_LIGHT_RED       "\033[91m"
+#define ANSI_LIGHT_GREEN     "\033[92m"
+#define ANSI_LIGHT_YELLOW    "\033[93m"
+#define ANSI_LIGHT_BLUE      "\033[94m"
+#define ANSI_LIGHT_CYAN      "\033[96m"
+
+#endif // ANSI_COLORS_H
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "ANSI_colors.h"
+
+#include <stdio.h>
+
+int main ()
+{
+        printf(ANSI_LIGHT_BLUE "# Square equation solver\n"
+                               "# (c) BAIDIUSENOV TIMUR, 2023\n\n" ANSI_DEFAULT_COLOR);
+
+        return 0;
+}
